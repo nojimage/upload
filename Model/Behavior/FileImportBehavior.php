447cb7162e1cb532610873183aa32d5c1967d21d
nojimage <nojimage@gmail.com>
2013-04-25 22:54:02 +0900

    fixes FileImportBehavior

--- Model/Behavior/FileImportBehavior.php
+++ Model/Behavior/FileImportBehavior.php
@@ -1,9 +1,44 @@
 <?php
 App::uses('UploadBehavior', 'Upload.Model/Behavior');
+App::uses('File', 'Utility');
+
 class FileImportBehavior extends UploadBehavior {
 
-	function handleUploadedFile($modelAlias, $field, $tmp, $filePath) {
-		return !rename($tmp, $filePath);
+	protected function _grab(Model $model, $field, $filePath) {
+		if (!is_file($filePath)) {
+			return false;
+		}
+
+		$file = new File($filePath);
+		$info = $file->info();
+
+		$model->data[$model->alias][$field] = array(
+			'name' => $info['basename'],
+			'type' => $info['mime'],
+			'tmp_name' => $filePath,
+			'error' => 0,
+			'size' => $info['filesize'],
+		);
+
+		return true;
+	}
+
+	public function beforeValidate(Model $model) {
+		foreach ($this->settings[$model->alias] as $field => $options) {
+			$filepath = $model->data[$model->alias][$field];
+			if (empty($model->data[$model->alias][$field])) {
+				continue;
+			}
+			if (!$this->_grab($model, $field, $filepath)) {
+				$model->invalidate($field, __d('upload', 'File not found.', true));
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function handleUploadedFile($modelAlias, $field, $tmp, $filePath) {
+		return copy($tmp, $filePath) && $this->_chmod($filePath, $this->settings[$modelAlias][$field]['mask']);
 	}
 
 }
